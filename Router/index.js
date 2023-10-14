08d427af76c64797c62fb7acec0137218ea15737
YaredAlex <yareda25@gmail.com>
2023-10-14 17:02:41 +0530

    adduser and getuser api added

--- Router/index.js
+++ Router/index.js
@@ -1,27 +1,52 @@
 import express from "express";
-import ItemModel from "../mongoose/userModel.js";
+import UserModel from "../mongoose/userModel.js";
+import UserService from "../service/userService.js";
 
 const ItemsRouting = () => {
   const router = express.Router();
-  router.get("/items", async (req, res) => {
+  // router.get("/items", async (req, res) => {
+  //   try {
+  //     const result = await UserModel.find({}).exec();
+  //     console.log(result);
+  //     res.json(result);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // });
+  router.get("/admin/users/get/:param", async (req, res) => {
     try {
-      const result = await ItemModel.find({}).exec();
-      console.log(result);
-      res.json(result);
+      const result = await UserService.getUser(param);
+      console.log(result, " ==> ", param);
     } catch (e) {
       console.log(e);
     }
   });
-  router.get("/items/put", async (req, res) => {
+  router.post("/admin/users/add", async (req, res) => {
+    const record = req.body;
+    console.log(record);
     try {
-      const newUser = await ItemModel.create({
-        id: "1234454",
-        f_name: "Yared",
+      const result = await UserService.create({
+        name: record?.name,
+        email: record?.email,
+        phone: record?.phone,
       });
-      console.log(newUser);
-      res.json(newUser);
+      console.log(result);
+      res.json({ message: "success" });
     } catch (e) {
-      res.json({ message: e.message });
+      console.log(e);
+      console.log(
+        e.errors?.name?.properties.message ||
+          e.errors?.email?.properties.message ||
+          e.errors?.phone?.properties.message,
+        " Getting error"
+      );
+      res.json({
+        message:
+          e.errors?.name?.properties.message ||
+          e.errors?.email?.properties.message ||
+          e.errors?.phone?.properties.message ||
+          "Email or phone Existes",
+      });
     }
   });
   return router;
