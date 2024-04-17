bcd6e252dbbd642b3b000aa7ac54b456b51f9e83
YaredAlex <yareda25@gmail.com>
2024-04-18 01:57:16 +0530

    db configuration'

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 import pg from "pg";
+import "dotenv/config.js";
 const { Pool } = pg;
-const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  database: "litend",
-  password: "root",
-  port: 5432,
-});
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+const pool = new Pool(config);
 
 export { pool };
