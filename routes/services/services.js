df87743769748eea1c91e1a9c0e5a9ac0dd9e473
YaredAlex <yareda25@gmail.com>
2024-06-02 03:30:30 +0530

    left join all users

--- routes/services/services.js
+++ routes/services/services.js
@@ -8,7 +8,7 @@ function serviceRoute() {
     //get all posts from DB
     try {
       const query = await pool.query(
-        "select first_name,profile_pic,phone,id,email,type from users inner join persona on users.id=persona.user_id;"
+        "select first_name,profile_pic,phone,id,email,type from users left join persona on users.id=persona.user_id;"
       );
       res.status(200).send(query.rows);
     } catch (e) {
